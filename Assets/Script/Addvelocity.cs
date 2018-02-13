b1ddef9e88eab424546ad56a7ff6eac2e7f57b01
Leonardo Bezerra <gunleobezerra@gmail.com>
2018-02-13 13:32:57 -0200

    Improving Planetary System

---
+++ Assets/Script/Addvelocity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addvelocity : MonoBehaviour {
+
+	private Rigidbody Body;
+	public float Velocity;
+	public float VelocityRate;
+
+	void Start(){
+		Body = this.gameObject.GetComponent<Rigidbody> ();
+
+	}
+
+	
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Z)) {
+			Velocity += VelocityRate;
+		} else if (Input.GetKey (KeyCode.X)) {
+			Velocity -= VelocityRate;
+		}
+
+		Body.AddForce (transform.TransformDirection(Vector3.right) * Velocity);
+	}
+}
---
+++ Assets/Script/GravityBody.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour {
+	[SerializeField]const float G = 9.8f;
+	[SerializeField]private float Gforce;
+	private float BodyMass,OtherBodyMass;
+	private Rigidbody OtherRigid;
+	[SerializeField]GameObject []OtherBodies;
+	int count = 0;
+	private Vector3 Pointto;
+
+	void Start () {
+		
+		BodyMass = this.gameObject.GetComponent<Rigidbody> ().mass;
+		OtherBodies = GameObject.FindGameObjectsWithTag ("Planet");
+
+
+	}
+	
+
+	void Update () {
+		Pointto = this.gameObject.transform.position - OtherBodies [count].transform.position;
+		if (count < OtherBodies.Length) {
+			OtherRigid = OtherBodies [count].GetComponent<Rigidbody> ();
+			OtherBodyMass =OtherRigid.mass;
+
+			Gforce = G * OtherBodyMass * BodyMass / Mathf.Pow (Vector3.Distance (OtherBodies [count].transform.position, this.gameObject.transform.position), 2);
+			OtherBodies [count].transform.localRotation = Quaternion.LookRotation (Pointto);
+				OtherRigid.AddForce (Pointto * Gforce, ForceMode.Acceleration);
+
+		}
+
+
+	}
+}
